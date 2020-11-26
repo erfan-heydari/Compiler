38ee70190ee454d73c997d892df4cd5e8d0f3fc9
Mostafa Tabrizi <legendmat3@gmail.com>
2020-11-26 16:48:20 +0330

    second commit2

--- src/compiler/CompilerView.java
+++ src/compiler/CompilerView.java
@@ -66,7 +66,12 @@ public class CompilerView extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("save");
+        jButton2.setText("save all");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -117,6 +122,10 @@ public class CompilerView extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
